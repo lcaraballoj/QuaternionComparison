22007686e93b92c1914413d7efcc4eb036c931da
Linnea Caraballo <linneacaraballo@Linneas-MacBook-Air.local>
2021-09-16 15:36:08 -0400

    Adding a second graph to show average difference

--- Comparison.py
+++ Comparison.py
@@ -11,10 +11,59 @@ from Quaternions import euler_to_quaternion
 
 ITERATIONS = 100
 
-# importing pandas library
-import pandas as pd
-# import matplotlib library
-import matplotlib.pyplot as plt
+# Function to graph all data
+def graphAll(time):
+    labels = ["Iteration", "Euler Time", "Quaternion Time"]     # Creating the labels
+
+    eQTimeDict = dict(zip(labels, zip(*time)))                  # Create dictionary
+
+    df = pd.DataFrame.from_dict(eQTimeDict)                # Use dictionary to create dataframs
+
+    # Plotting graph
+    df.plot.bar(x="Iteration", y=["Euler Time", "Quaternion Time"], stacked = True, width = 1)
+
+    # Return the figure
+    return plt.figure(1)
+
+# Function to graph average times
+def graphAvg(eTime, qTime):
+    # Getting averages of compute time for euler and quaternion
+    eAvg = numpy.average(eTime)
+    qAvg = numpy.average(qTime)
+
+    #Calculate standard deviation
+    eStd = numpy.std(eTime)
+    qStd = numpy.std(qTime)
+
+    e = [min(eTime), max(eTime)]
+    q = [min(qTime), max(qTime)]
+
+    print(e)
+    print(q)
+
+    # Define labels, x_pos, values, error
+    labels = ["Euler Average Time", "Quaternion Average Time"]
+    x_pos = numpy.arange(len(labels))
+    values = [eAvg, qAvg]
+    error = [eStd, qStd]
+
+    # Build the plot
+    fig, ax = plt.subplots()
+    ax.bar(x_pos, values,
+       yerr=error,
+       align='center',
+       alpha=0.5,
+       ecolor='black',
+       capsize=10)
+    ax.set_ylabel("Computation Time")
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(labels)
+    ax.set_title("Computation Comparison")
+    ax.yaxis.grid(True)
+
+    # Return the figure
+    return plt.figure(2)
+
 
 
 def main():
@@ -27,7 +76,7 @@ def main():
     i = 0
 
     while i<=ITERATIONS:
-        startEuler = timeit.default_timer()          #Start timer for euler caluclation run time
+        startEuler = timeit.default_timer()          # Start timer for euler caluclation run time
 
         #Generate random angles
         phi = math.pi / randint(1, 6)
@@ -38,27 +87,30 @@ def main():
         #print("theta = ", theta)
         #print("psi = ", psi)
 
-        euler = X(phi) * Y(theta) * Z(theta)       #Calculate rotation matrix
-        stopEuler = timeit.default_timer()         #Stop timer for euler calculation run time
+        euler = X(phi) * Y(theta) * Z(theta)       # Calculate rotation matrix
+        stopEuler = timeit.default_timer()         # Stop timer for euler calculation run time
 
-        #print("\n", numpy.round(euler, decimals = 2))       #Print rotation matrix
-        #print("\nTime: ", stopEuler - startEuler, "\n")     #Print run time
+        #print("\n", numpy.round(euler, decimals = 2))       # Print rotation matrix
+        #print("\nTime: ", stopEuler - startEuler, "\n")     # Print run time
 
         startQuaternion = timeit.default_timer()
 
         quaternion = euler_to_quaternion(phi, theta, psi)
-        stopQuaternion = timeit.default_timer()             #Stop timer for run time
+        stopQuaternion = timeit.default_timer()             # Stop timer for run time
 
-        #print("\n", numpy.round(quaternion, decimals = 2))      #Print rotation matrix
-        #print("\nTime: ", stopQuaternion - startQuaternion)     #Print run time
+        #print("\n", numpy.round(quaternion, decimals = 2))      # Print rotation matrix
+        #print("\nTime: ", stopQuaternion - startQuaternion)     # Print run time
 
         add = table.add_row([phi, theta, psi, numpy.round(euler, decimals = 2), stopEuler - startEuler, stopQuaternion - startQuaternion, numpy.round(quaternion, decimals  = 2)])  #Add information to table
 
-        result = [phi, theta, psi, euler, stopEuler - startEuler, stopQuaternion - startQuaternion, quaternion]              #Add information to first array
+        result = [phi, theta, psi, euler, stopEuler - startEuler, stopQuaternion - startQuaternion, quaternion]              # Add information to first array
 
         final.append(result)            #Append first array to final array
 
         eQTime = [i, stopEuler - startEuler, stopQuaternion - startQuaternion]
+
+        eTime = [stopEuler - startEuler]
+        qTime = [stopQuaternion - startQuaternion]
 
         time.append(eQTime)
 
@@ -66,25 +118,14 @@ def main():
 
     print(table)
 
-    dictionary = dict(zip(headers, zip(*final)))    #Convert final array to a dictionary
+    dictionary = dict(zip(headers, zip(*final)))    # Convert final array to a dictionary
 
-    dfTable = pd.DataFrame.from_dict(dictionary)                            #Define the dataframes
-    dfTable.to_csv("Comparison.csv", index = False, header = True)         #Write dataframes to csv to be saved
+    dfTable = pd.DataFrame.from_dict(dictionary)                            # Define the dataframes
+    dfTable.to_csv("Comparison.csv", index = False, header = True)         # Write dataframes to csv to be saved
 
-    labels = ["Iteration", "Euler Time", "Quaternion Time"]
-
-    eQTimeDict = dict(zip(labels, zip(*time)))
-
-    dfGraph = pd.DataFrame.from_dict(eQTimeDict)
-
-    print(dfGraph)
-    #print(dfTable)
-
-    # plotting graph
-    dfGraph.plot.bar(x="Iteration", y=["Euler Time", "Quaternion Time"], stacked = True, width = 1)
+    graphAll(time)
+    graphAvg(eTime, qTime)
 
     plt.show()
-
-    #graph(label_array(), eTime, qTime)
 
 main()
