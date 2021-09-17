f9ba1930de2ee952dce7dc51b1fbb5aa55071a85
Linnea Caraballo <linneacaraballo@linneas-air.sacredheart.edu>
2021-09-17 14:37:08 -0400

    Adding new file for graph functions

--- Comparison.py
+++ Comparison.py
@@ -8,63 +8,9 @@ from random import *
 from prettytable import PrettyTable
 from EulerAngle import X, Y, Z
 from Quaternions import euler_to_quaternion
+from Graphs import graphAll, graphAvg
 
-ITERATIONS = 100
-
-# Function to graph all data
-def graphAll(time):
-    labels = ["Iteration", "Euler Time", "Quaternion Time"]     # Creating the labels
-
-    eQTimeDict = dict(zip(labels, zip(*time)))                  # Create dictionary
-
-    df = pd.DataFrame.from_dict(eQTimeDict)                # Use dictionary to create dataframs
-
-    # Plotting graph
-    df.plot.bar(x="Iteration", y=["Euler Time", "Quaternion Time"], stacked = True, width = 1)
-
-    # Return the figure
-    return plt.figure(1)
-
-# Function to graph average times
-def graphAvg(eTime, qTime):
-    # Getting averages of compute time for euler and quaternion
-    eAvg = numpy.average(eTime)
-    qAvg = numpy.average(qTime)
-
-    #Calculate standard deviation
-    eStd = numpy.std(eTime)
-    qStd = numpy.std(qTime)
-
-    e = [min(eTime), max(eTime)]
-    q = [min(qTime), max(qTime)]
-
-    print(e)
-    print(q)
-
-    # Define labels, x_pos, values, error
-    labels = ["Euler Average Time", "Quaternion Average Time"]
-    x_pos = numpy.arange(len(labels))
-    values = [eAvg, qAvg]
-    error = [eStd, qStd]
-
-    # Build the plot
-    fig, ax = plt.subplots()
-    ax.bar(x_pos, values,
-       yerr=error,
-       align='center',
-       alpha=0.5,
-       ecolor='black',
-       capsize=10)
-    ax.set_ylabel("Computation Time")
-    ax.set_xticks(x_pos)
-    ax.set_xticklabels(labels)
-    ax.set_title("Computation Comparison")
-    ax.yaxis.grid(True)
-
-    # Return the figure
-    return plt.figure(2)
-
-
+ITERATIONS = 1000
 
 def main():
     final = []
